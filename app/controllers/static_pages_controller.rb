59794a2b8ceee850335b26304456e733f27d0dbd
Nicolas Mauchle <nmauchle@gmail.com>
2014-08-12 14:11:01 +0200

    Add images

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,13 @@ class StaticPagesController < ApplicationController
   end
 
   def photos
+    @littering_im_waldkurs = Dir.glob("app/assets/images/gallery/littering_im_waldkurs/*tb*")
+    @kompostkurs_kindergarten = Dir.glob("app/assets/images/gallery/kompostkurs_kindergarten/*tb*")
+    @werken_mit_abfall = Dir.glob("app/assets/images/gallery/werken_mit_abfall/*tb*")
+    @anti_littering = Dir.glob("app/assets/images/gallery/anti_littering/*tb*")
+    @sek = Dir.glob("app/assets/images/gallery/anti_littering/sek/*tb*")
+    @sek_bi = Dir.glob("app/assets/images/gallery/anti_littering/sek_bi/*tb*")
+    @sonstiges = Dir.glob("app/assets/images/gallery/sonstiges/*tb*")
   end
 
   def videos
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+
+	def get_original(image, category)
+		get_image_path(image, category).gsub!(/_([a-z]+).jpg/, '_og.jpg')
+	end
+
+	def get_image_path(image, category)
+		"gallery/#{category}/#{image.split('/').last}"
+	end
 end
