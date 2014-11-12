3514b76bdea456e874e332b13c78610bbbd23043
Nicolas Mauchle <nmauchle@gmail.com>
2014-11-12 21:42:03 +0100

    Add Button for course sign up

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -14,7 +14,7 @@ class Contact < MailForm::Base
   def headers
     {
       subject: "Neue Anmeldung für '#{course}'",
-      to: "nmauchle@gmail.com",
+      to: "nmauchle@gmail.com", # barbara.schumacher@bluewin.ch
       from: %("#{name_teacher}" <#{email}>)
     }
   end
