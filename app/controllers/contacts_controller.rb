c443d09e28d73917d480ca5a935c507a7e8d4998
Nicolas Mauchle <nicolas@nicolasmauchle.ch>
2018-07-29 15:48:26 +0200

    Clean up homepage

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,37 +1,35 @@
+# coding: utf-8
 class ContactsController < ApplicationController
 
 	def new
 		@contact = Contact.new
 		@contact.course = []
 		@contact.course << "Kompostieren, die Tierwelt im Kompost / 2 Std."
-		@contact.course <<  "Recycling und Kompostieren / 2 Std."
+		@contact.course << "Recycling und Kompostieren / 2 Std."
 		@contact.course << "Kompostmobil / 2‒3 Std."
 		@contact.course << "Umwelttheater Figurenspiel 1: Kompostieren + Littering / 2 Std."
+                @contact.course << "Figurenspiel Madame Poubelle, Recycling + vermeiden / 2 Std."
 		@contact.course << "Schulgarten-Projekt"
 		@contact.course << "Recycling, Kompostieren + erneuerbare Energie durch Biomassenvergärung (Biopower Pratteln) / 3 Std."
 		@contact.course << "Littering in der Gemeinde + Naturkreisläufe im Wald / 3,5 Std."
 		@contact.course << "Erzähltheater: Fee Wunderfitz + Littering  / 2Std."
 		@contact.course << "Abfallvermeidung in der Kehrichtverbrennung Basel / 3 Std."
-		@contact.course << "Tipps und Hintergründe zum täglichen Abfallvermeiden / 2Std."
-		@contact.course << "Figurenspiel 2:Littering Recycling / 2‒3Std."
+                @contact.course << "Abfallmorgen: Vermeiden, Recycling, Littering"
+		@contact.course << "Tipps und Hintergründe zum täglichen Abfallvermeiden / 2 Std."
+		@contact.course << "Figurenspiel 2:Littering Recycling / 2‒3 Std."
+                @contact.course << "Elektroschrott vermeiden und Recycling 2 Std."
 		@contact.course << "Abfallvermeidung, Recycling, Basteln mit Abfall / 2 Std."
-		@contact.course << "Recycling, Abfallvermeidung, Werken mit Abfall / 3 Std."
 		@contact.course << "Glas Recycling + Ressourcenschonung / 2 Std."
 		@contact.course << "Papier Herstellung + Recycling ‒ neue Seiten / 2 Std."
 		@contact.course << "Littering, Recycling auf Werkhof / 3 Std."
-		@contact.course << "Anti-Littering Workshop auf dem Werkhof – Oberstufe/ 3 Std."
 		@contact.course << "Littering ums Schulhaus und in der Gemeinde / 2 Std."
 		@contact.course << "Anti-Littering-Einsatz in der Gemeinde / 4 Std."
 		@contact.course << "Abfall im Wasser / 2‒3 Std."
 		@contact.course << "Bachputzede / 4 Std."
-		@contact.course << "Trinkwasseraufbereitung in der Langen Erlen, Basel / 3,5 Std."
-		@contact.course << "Abfallwettspiele / 4 Std."
-		@contact.course << "Aufbaukurs Energieeffizienz"
-		@contact.course << "Einführungskurs Energieeffizienz / 2 Std."
-		@contact.course << "Projekttag oder Projektwochen"
+		@contact.course << "Ein Morgen rund um das Thema Wasser."
+		@contact.course << "Energieeffizienz Einführungskurs / 2 Std."
 		@contact.course << "Umwelteinsätze"
-		@contact.course << "Eltern- oder Eltern-Kind-Kurse"
-		@contact.course << "LehrerInnenweiterbildung"
+                @contact.course << "Projekttag oder Projektwochen"
 	end
 
 	def create
